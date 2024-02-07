0f29fc6470b75d55c14416346ec3cac2ca293f40
simon eagar <simonrex@eagar.net>
2024-02-06 22:50:53 -0800

    Commented main script. Moved obsolete scripts to new folder.

--- script/GoLPG.py
+++ script/GoLPG.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import time
 
+#initialize pygame widow and global variables
 pygame.init()
 pygame.display.set_caption("GameOfLife")
 w = 1200
@@ -18,6 +19,8 @@ class GameOfLife:
         self.dx = 12
         self.dy = 12
         
+        # intialize cells
+        # 50/50 chance to start alive or dead
         self.cells = [[0 for y in range(self.h)] for x in range(self.w)]
         for x in range(self.w):
             for y in range(self.h):
@@ -29,11 +32,13 @@ class GameOfLife:
                     self.cells[x][y] = 0
 
     def tick(self):
+        # prepare a new array for updates so tick is global
         new_cells = [[0 for y in range(self.h)] for x in range(self.w)]
         
         for x in range(0,self.w):
             for y in range(0,self.h):
                 cell = self.cells[x][y]
+                # count neighbors, not including self
                 neighbors = 0 - cell
                 for i in range(-1,2):
                     for j in range(-1,2):
@@ -46,25 +51,34 @@ class GameOfLife:
                     new_cells[x][y] = 0
                     screen.fill((0,0,0),(x*self.dx,y*self.dy,self.dx,self.dy))
         
+        # keep updated array
         self.cells = new_cells
     
     def flip(self, pos):
+        # find cell by integer dividing click position by cell size
         x = pos[0] // self.dx
         y = pos[1] // self.dy
+        # inline XOR, 0 becomes 1, 1 becomes 0
         self.cells[x][y] ^= 1
         screen.fill((0,255*self.cells[x][y],0),(x*self.dx,y*self.dy,self.dx,self.dy))
 
+# initialize game object, print directions
 life = GameOfLife(100, 80)
 print("Mouse click to toggle cells")
 print("Left/Right arrows to control tick rate")
     
 while 1:
+    # tick after waiting sleep_time seconds
     if time.time() >= last_tick + sleeps[sleep_time]:
         life.tick()
+        print((time.time()-last_tick)*1000)
         last_tick = time.time()
     
+    # handle each event
     for event in pygame.event.get():
         # handle time changes
+        # possible wait times are pulled from a global list
+        # max FPS, 10 FPS, 4 FPS, 2 FPS, 1 FPS, frozen
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 sleep_time = max(sleep_time - 1, 0)
